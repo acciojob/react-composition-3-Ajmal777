6f7b266e31750544e54c758d2d6b01aae6a44313
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-17 13:08:16 +0000

    fixed structure

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -4,14 +4,14 @@ const Tooltip = ({children, text}) =>{
     const [hover, setHover] = useState(false);
 
     return (
-        <div 
+        <h2 
             className="tooltip"
             onMouseEnter={()=>setHover(true)}
             onMouseLeave={()=>setHover(false)}
         >
             {children}
             { hover && (<div className="tooltiptext"> {text} </div>) }
-        </div>
+        </h2>
     )
 }
 
