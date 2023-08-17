4e4dc502fd2fde9aa4f0beabc1ad9aaa89a1db66
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-17 13:56:38 +0000

    submitting code-Thu Aug 17 2023 13:56:34 GMT+0000 (Coordinated Universal Time)

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -5,13 +5,17 @@ const Tooltip = () =>{
 
     return (
         <div>
-            <h2 className="tooltip" onMouseEnter={()=> setHover('h2')} onMouseLeave={()=> setHover('p')}>
-                {hover === 'h2' && <div className="tooltiptext">This is a tooltip</div>}
-                Hover over me
+            <h2 className="tooltip" >
+                <div onMouseEnter={()=> setHover('h2')} onMouseLeave={()=> setHover('')}>
+                    {hover === 'h2' && <div className="tooltiptext">This is a tooltip</div>}
+                    Hover over me
+                </div>
             </h2>
-            <p className="tooltip" onMouseEnter={() => setHover('p')} onMouseLeave={() => setHover('h2')}>
-                {hover === 'p' && <div className="tooltiptext">This is another tooltip</div>}
-                Hover over me to see another tooltip
+            <p className="tooltip" >
+                <div onMouseEnter={() => setHover('p')} onMouseLeave={() => setHover('')}>
+                    {hover === 'p' && <div className="tooltiptext">This is another tooltip</div>}
+                    Hover over me to see another tooltip
+                </div>
             </p>
         </div>
     )
